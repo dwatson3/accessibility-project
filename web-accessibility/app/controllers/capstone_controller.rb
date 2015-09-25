9fe50c502ff25bdb40bdd6c4aaecd32ea7378413
Daphne Watson <daphne.renee.laurels@gmail.com>
2015-09-25 16:43:55 -0700

    twilio api added

--- web-accessibility/app/controllers/capstone_controller.rb
+++ web-accessibility/app/controllers/capstone_controller.rb
@@ -5,6 +5,28 @@ def index
 end	
 
 def twilio
+
+	if params[:q]
+
+		require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+		require 'twilio-ruby'
+
+		# put your credentials here
+		account_sid = 'AC59ab62afc51d409ec8848087d997a822'
+		auth_token = 'e3e0337ebd1836041fd1210bbc9d520e'
+
+		# set up a client to talk to the Twilio REST API
+		@client = Twilio::REST::Client.new account_sid, auth_token
+
+		message = @client.account.messages.create(:body => params[:q],
+			:to => "+15303015894",
+			:from => "+15307630890")
+
+		puts message.sid
+
+		@textinformation = params[:q]
+
+	end
 	render "welcome/twilio"
 end
 
